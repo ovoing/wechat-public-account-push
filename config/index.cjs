95d98fee8ef4a00e323230a02267eae07b9c66be
ovoing <115028482+ovoing@users.noreply.github.com>
2022-10-05 20:12:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oijNz6OTnAHa_piMXLKCKBe4rUho',
+      id: 'oijNz6PVV0up3RgXzBnCa-YVCzlg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'TZ9QS1QkPG03UyWgoTrYEFxQoS9NaJ-6kBlR72nAMdI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
